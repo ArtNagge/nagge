bc16730e2c7aae6fa15671278bfdadcc4a9a3c67
Yan <artnagge@gmail.com>
2016-08-02 11:12:22 +0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
 //Toggle Menu
 
-$('#toggle-menu-btn').click(function () {
-	$('#toggle-menu').addClass('toggle-visible');
+$('#id кнопки').click(function () {
+	$('#id меню').addClass('class для отображения меню');
 });
-$('#close-toggle').click(function () {
-	$('#toggle-menu').removeClass('toggle-visible');
+$('#id кнопки').click(function () {
+	$('#id меню').removeClass('class для отображения меню');
 });
 
 //Перенаправление при клике
 
-$("#btn-portfolio").click(function(){
+$("#id кнопки").click(function(){
 	setTimeout(function () {
-		location.href='http://nagge/'
+		location.href='http://ссылка'
 	}, 150);
 });
 
@@ -19,153 +19,18 @@ $("#btn-portfolio").click(function(){
 
 $(window).scroll(function () {
 
-	var parallaxWindowScroll = $(this).scrollTop();
+	var (название переменной) = $(this).scrollTop();
 
-	//function animated - #about
+	if ((название переменной) > $('#id блока').offset().top - ($(window).height() / 1.5)) {
 
-	if (parallaxWindowScroll > $('#about').offset().top - ($(window).height() / 1.5)) {
-
-		$('#about h4').each(function(i){
+		$('(#)(.)элемент анимации').each(function(i){
 
 			setTimeout(function(){
-				$('#about h4').eq(i).addClass('animated fadeInLeft');
+				$('(#)(.)элемент анимации').eq(i).addClass('animated fadeInLeft');  // <-- реализовано с плагином aAnimate css
 			}, 100 * (i+1));
 
 		});
 
-		$('#about .black-line-about').each(function(i){
-
-			setTimeout(function(){
-				$('#about .black-line-about').eq(i).addClass('animated fadeInRight');
-			}, 150 * (i+1));
-
-		});
-
 	}
 
-	if (parallaxWindowScroll > $('#about').offset().top - ($(window).height() / 1.7)) {
-
-		$('.p-visible').each(function(i){
-
-			setTimeout(function(){
-				$('.p-visible').eq(i).addClass('animated fadeInLeft');
-			}, 250 * (i+1));
-
-		});
-
-		$('.img-visible').each(function(i){
-
-			setTimeout(function(){
-				$('.img-visible').eq(i).addClass('animated fadeInRight');
-			}, 250 * (i+1));
-
-		});
-
-	}
-
-	//function animated - #services
-
-	if (parallaxWindowScroll > $('#services').offset().top - ($(window).height() / 1.5)) {
-
-		$('#services h4').each(function(i){
-
-			setTimeout(function(){
-				$('#services h4').eq(i).addClass('animated fadeInRight');
-			}, 100 * (i+1));
-
-		});
-
-		$('#services .line-services').each(function(i){
-
-			setTimeout(function(){
-				$('#services .line-services').eq(i).addClass('animated fadeInUp');
-			}, 350 * (i+1));
-
-		});
-
-	}
-
-	if (parallaxWindowScroll > $('#services').offset().top - ($(window).height() / 1.7)) {
-
-		$('.p-services').each(function(i){
-
-			setTimeout(function(){
-				$('.p-services').eq(i).addClass('animated fadeInUp');
-			}, 250 * (i+1));
-
-		});
-
-		$('.sentence').each(function(i){
-
-			setTimeout(function(){
-				$('.sentence').eq(i).addClass('animated fadeInUp');
-			}, 650 * (i+1));
-
-		});
-
-	}
-
-	//function animated - #last-gob
-
-	if (parallaxWindowScroll > $('#last-job').offset().top - ($(window).height() / 1.5)) {
-
-		$('#last-job h4').each(function(i){
-
-			setTimeout(function(){
-
-				$('#last-job h4').eq(i).addClass('animated fadeInRight');
-
-			}, 100 * (i+1));
-
-		});
-
-		$('.black-line-last-job').each(function(i) {
-
-			setTimeout(function() {
-
-				$('.black-line-last-job').eq(i).addClass('animated fadeInUp');
-
-			}, 350 * (i+1));
-
-		});
-
-	}
-
-	if (parallaxWindowScroll > $('#last-job').offset().top - ($(window).height() / 1.7)) {
-
-		$('.job').each(function(i) {
-
-			setTimeout(function() {
-
-				$('.job').eq(i).addClass('animated fadeInRight');
-
-			}, 350 * (i+1));
-
-		});
-
-		$('.job-content').each(function(i) {
-
-			setTimeout(function() {
-
-				$('.job-content').eq(i).addClass('animated fadeInUp');
-
-			}, 650 * (i+1));
-
-		});
-
-	}
-
-	if (parallaxWindowScroll > $('#last-job').offset().top - ($(window).height() / 2.5)) {
-
-		$('#btn-portfolio').each(function(i) {
-
-			setTimeout(function() {
-
-				$('#btn-portfolio').eq(i).addClass('animated fadeInLeft');
-
-			}, 350 * (i+1));
-
-		});
-
-	}
 });
